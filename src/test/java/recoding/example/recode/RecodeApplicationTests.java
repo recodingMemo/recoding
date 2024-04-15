8619439ea5f734d27fcabfc77c8c2a008d090428
hwjk16322@gmail.com <hwjk16322@gmail.com>
2024-04-16 00:13:27 +0900

    first commit

---
+++ src/test/java/recoding/example/recode/RecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package recoding.example.recode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
